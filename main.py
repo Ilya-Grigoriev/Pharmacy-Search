911bfa1446776c8b104b6e8a913d08ab88ab6034
Ilya-Grigoriev <iliagrigoryev@yandex.ru>
2022-03-20 14:04:46 +0300

    First

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+from io import BytesIO
+import requests
+from PIL import Image
+from distance import lonlat_distance
+
+place = input('Введите адрес: ')
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": place,
+    "format": "json"}
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+response = requests.get(geocoder_api_server, params=geocoder_params)
+if not response:
+    pass
+json_response = response.json()
+toponym = json_response["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]
+place_coord = toponym["Point"]["pos"]
+lon, lat = place_coord.split()
+
+metro_request = 'https://search-maps.yandex.ru/v1/'
+params = {
+    'apikey': '611ec11a-6b32-42d2-8883-40ffb48f0ef0',
+    'lang': 'ru_RU',
+    'text': 'аптека',
+    'll': ','.join([lon, lat])
+}
+response = requests.get(metro_request, params=params).json()
+pharmacy_coord = list(map(str, response['features'][0]['geometry']['coordinates']))
+map_params = {
+    "l": "map",
+    'pl': f'{",".join(pharmacy_coord)},{",".join([lon, lat])}',
+    'pt': f'{",".join(pharmacy_coord)},pm2dbm1~{",".join([lon, lat])},pm2rdm2'
+}
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+response = requests.get(map_api_server, params=map_params)
+Image.open(BytesIO(
+    response.content)).show()
+print(lonlat_distance(map(float, pharmacy_coord), map(float, [lon, lat])))
